2010f686caf647b0a4d45076cdb5cce3b9a248a4
Deepti Rathore <deepti@Deeptis-MacBook-Pro.local>
2021-09-09 15:19:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+**Steps**
+1. docker build -t springboot-postgres-k8s:latest .
+2. cd src/main/resources
+3. kubectl apply -f postgres-configmap.yml
+4. kubectl apply -f postgres-credentials.yml
+5. kubectl apply -f postgres-deployment.yml
+6. kubectl apply -f deployment.yml
---
+++ src/main/java/com/springrest/springbootdocker/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.springrest.springbootdocker.repository;
+
+import com.springrest.springbootdocker.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+}
